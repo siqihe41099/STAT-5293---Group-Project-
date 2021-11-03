fd2a4c316a4eeb321c2942bab182643b7aa75f36
Weiyi Jiang <31546415+weiyi-jiang@users.noreply.github.com>
2021-11-03 13:08:38 -0400

    Create irisNormalization.py

---
+++ irisNormalization.py
@@ -0,0 +1,24 @@
+def irisNormalization(x_i, y_i, r_i, x_p, y_p, r_p, img_in):
+  # I_n is a M x N (64 x 512 in this experiment) normalized image
+  M, N = 64, 512
+  # Create an empty matrix to store the normalized image
+  normalized = np.zeros((M, N))
+
+  # thetas are the angles from 0 to 2*pi
+  thetas = np.arange(N)/N * 2 * np.pi
+
+  # Project the original iris into new coordinate system defined by LiMa's paper
+  for i in range(M):
+    for j in range(N):
+
+      x_p_theta = x_p + r_p * np.cos(thetas[j]) 
+      y_p_theta = y_p - r_p * np.sin(thetas[j]) 
+      x_i_theta = x_p + r_i * np.cos(thetas[j]) 
+      y_i_theta = y_p - r_i * np.sin(thetas[j]) 
+      x, y = x_p_theta + (x_i_theta - x_p_theta) * i / M, y_p_theta + (y_i_theta - y_p_theta) * i / M
+
+      x = min(319, x) or max(0, x)
+      y = min(279, y) or max(0, y)
+      normalized[i, j] = img_in[int(y), int(x)]
+
+  return(normalized)
