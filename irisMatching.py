76247afeeeb341a1b3624a62a0f2903e6995a717
Weiyi Jiang <31546415+weiyi-jiang@users.noreply.github.com>
2021-11-03 13:28:03 -0400

    Update irisMatching.py

--- irisMatching.py
+++ irisMatching.py
@@ -1,8 +1,11 @@
 from sklearn.neighbors.nearest_centroid import NearestCentroid
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as lda
+import cv2
+import numpy as np
+
 def irisMatching(x_train, y_train, x_test, y_test, dimension=107,reduce=True):
   if reduce == False:
-    metric=["l1","l2","cosine"]
+    metric=["manhattan","euclidean","cosine"]
     score = []
     for i in metric:
       classifier = NearestCentroid(metric=i)
