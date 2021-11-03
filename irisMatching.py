e8184b5eda48025a8a5deb4f6a3cd82b6d75fae7
siqihe41099 <92268046+siqihe41099@users.noreply.github.com>
2021-11-03 15:54:49 -0400

    Update irisMatching.py

--- irisMatching.py
+++ irisMatching.py
@@ -4,14 +4,18 @@ import cv2
 import numpy as np
 
 def irisMatching(x_train, y_train, x_test, y_test, dimension=107,reduce=True):
-  if reduce == False:
+  if reduce == False: # No dimension reduction
+    # We apply three metrics here: l1, l2 and Cosine.
     metric=["manhattan","euclidean","cosine"]
     score = []
     for i in metric:
+      # Reference: https://scikit-learn.org/stable/modules/generated/sklearn.neighbors.NearestCentroid.html
       classifier = NearestCentroid(metric=i)
       classifier.fit(x_train, y_train)
       score.append(classifier.score(x_test, y_test))
     return(score, x_train, x_test, classifier.centroids_)
+  # Reference: https://scikit-learn.org/stable/modules/generated/sklearn.discriminant_analysis.LinearDiscriminantAnalysis.html
+  
   clf = lda(n_components=dimension)
   fit = clf.fit(x_train,y_train)
   X_train = fit.transform(x_train)
