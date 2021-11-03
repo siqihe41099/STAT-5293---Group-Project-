7c3e92e5f3a714ec3baa0c0bdf0a7790cffc0240
Weiyi Jiang <31546415+weiyi-jiang@users.noreply.github.com>
2021-11-03 15:51:31 -0400

    Update irisRecognition.py

--- irisRecognition.py
+++ irisRecognition.py
@@ -4,12 +4,12 @@ import matplotlib.pyplot as plt
 from sklearn.neighbors.nearest_centroid import NearestCentroid
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as lda
 
-from IrisLocalization  import *
-from IrisNormalization import *
-from ImageEnhancement import *
-from FeatureExtraction import *
-from IrisMatching      import *
-from PerformanceEnvaluation import *
+from irisLocalization  import *
+from irisNormalization import *
+from irisEnhancement import *
+from featureExtraction import *
+from irisMatching      import *
+from performanceEnvaluation import *
 
 def irisRecognition(imgs_train,imgs_test):
   size_train = len(imgs_train)
